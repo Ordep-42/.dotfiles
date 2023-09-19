6bb2a3f7b3e739658c7424f81fda7faa13b2d85b
Ordep-42 <p.galvao@outlook.com.br>
2023-09-19 08:44:13 -0300

    Initial setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sudo apt-get update
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5 -y -y -y -y -y -y -y -y -y
