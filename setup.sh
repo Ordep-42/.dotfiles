f311acf7b34d5ace7358f5f76219fa66e79828b4
Ordep-42 <90785456+Ordep-42@users.noreply.github.com>
2024-04-10 15:23:29 -0300

    Redo setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/bash
-
-sudo apt-get update
-sudo apt update
-sudo apt upgrade
-
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.nanorc ~/.nanorc
-
-git clone https://github.com/tsouanas/lookbusy.git ~/lookbusy
